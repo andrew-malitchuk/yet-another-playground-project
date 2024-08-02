50f14bf0f4cdbea9829c0ac53c91d806337bc69f
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-08-02 19:53:28 +0300

    feat: add di

---
+++ 2048/src/main/kotlin/dev/yapp/ya2048cc/data/source/preferences/impl/di/PreferencesModule.kt
@@ -0,0 +1,44 @@
+package dev.yapp.ya2048cc.data.source.preferences.impl.di
+
+import dev.yapp.ya2048cc.data.source.preferences.HistoryPreferencesDataSource
+import dev.yapp.ya2048cc.data.source.preferences.GamePreferencesDataSource
+import dev.yapp.ya2048cc.data.source.preferences.ScorePreferencesDataSource
+import dev.yapp.ya2048cc.data.source.preferences.impl.HistoryPreferencesDataSourceImpl
+import dev.yapp.ya2048cc.data.source.preferences.impl.GamePreferencesDataSourceImpl
+import dev.yapp.ya2048cc.data.source.preferences.impl.ScorePreferencesDataSourceImpl
+import dev.yapp.ya2048cc.data.source.preferences.impl.dao.BarDao
+import dev.yapp.ya2048cc.data.source.preferences.impl.dao.FooDao
+import dev.yapp.ya2048cc.data.source.preferences.impl.dao.HistoryDao
+import dev.yapp.ya2048cc.data.source.preferences.impl.dao.GameDao
+import dev.yapp.ya2048cc.data.source.preferences.impl.dao.HistoryDaoNew
+import dev.yapp.ya2048cc.data.source.preferences.impl.dao.ScoreDao
+import org.koin.dsl.module
+
+val preferencesModule = module {
+
+    single { HistoryDao(get()) }
+    single { GameDao(get()) }
+    single { ScoreDao(get()) }
+    single { FooDao(get()) }
+    single { BarDao(get()) }
+    single { HistoryDaoNew(get()) }
+
+    single<HistoryPreferencesDataSource> {
+        HistoryPreferencesDataSourceImpl(
+            get(),
+        )
+    }
+
+    single<GamePreferencesDataSource> {
+        GamePreferencesDataSourceImpl(
+            get(),
+        )
+    }
+
+    single<ScorePreferencesDataSource> {
+        ScorePreferencesDataSourceImpl(
+            get(),
+        )
+    }
+
+}
