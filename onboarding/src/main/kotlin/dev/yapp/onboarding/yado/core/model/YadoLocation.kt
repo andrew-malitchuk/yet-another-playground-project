d53f700873c1378c7da8bd80521d76b4b801cd6d
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-12-25 13:20:59 +0200

    feat: play around with bitmap

---
+++ onboarding/src/main/kotlin/dev/yapp/onboarding/yado/core/model/YadoLocation.kt
@@ -0,0 +1,9 @@
+package dev.yapp.onboarding.yado.core.model
+
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.unit.IntSize
+
+data class YadoLocation(
+    val offset: Offset,
+    val size: IntSize
+)
