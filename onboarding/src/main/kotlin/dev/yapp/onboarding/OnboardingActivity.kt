77a896a9a77b96386a0b1fa0af08b01caa74aeb1
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-12-31 13:21:32 +0200

    feat: play around with bitmap

--- onboarding/src/main/kotlin/dev/yapp/onboarding/OnboardingActivity.kt
+++ onboarding/src/main/kotlin/dev/yapp/onboarding/OnboardingActivity.kt
@@ -1,16 +1,11 @@
 package dev.yapp.onboarding
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
+import dev.yapp.onboarding.yado.YadoContent
+import dev.yapp.onboarding.yado.core.rememberYadoState
 
 class OnboardingActivity : ComponentActivity() {
 
@@ -18,7 +13,15 @@ class OnboardingActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            OnboardingContent()
+
+            val yadoState = rememberYadoState()
+
+            DemoContent(yadoState) {
+                YadoContent(
+                    it,
+                    yadoState
+                )
+            }
         }
     }
 }
